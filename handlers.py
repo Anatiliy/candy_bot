a06a42f8f03baf5edf2aa7aa7e65dbd196dbded3
Anatoliy <staheev.a.n@yandex.ru>
2023-02-04 20:23:02 +0600

    изментл функцию mes_all, написал функцию mes_game

--- handlers.py
+++ handlers.py
@@ -1,13 +1,17 @@
-from random import randint
 from loader import dp
 from aiogram import types
 from candybot import CandyBot
 
 game = CandyBot()
 
-# запуск игры
+# запуск бота
 @dp.message_handler(commands=['start'])
 async def mes_start(message: types.Message):
+    await message.answer(f'Привет, {message.from_user.first_name}! В "конфеты" играть будем?\nУсловия игры:\nВ вазе лежит заданное количество конфет. Играют два игрока делая ход друг после друга. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход.\nЖми /game , если согласен.')
+
+# запуск игры
+@dp.message_handler(commands=['game'])
+async def mes_game(message: types.Message):
     game.start = True
     if game.count: # если сыграли хотябы одну игру
         if game.win: # если в прошлой игре победил бот
@@ -37,14 +41,14 @@ async def mes_all(message: types.Message):
                 game.start = False
                 game.count += 1
                 game.win = 0
-                await message.answer(f'Ты победил!! Твой приз {game.total} конфет!!! Желаешь повторить триумф - Жми /start')
+                await message.answer(f'Ты победил!! Твой приз {game.total} конфет!!! Желаешь повторить триумф - Жми /game')
             else:
                 botnum = game.move()
                 if game.number == 0:
                     game.start = False
                     game.count += 1
                     game.win = 1
-                    await message.answer(f'Я забрал {botnum} конфет и победил!!! Мой приз {game.total} конфет!!! Желаешь реванша - жми /start')
+                    await message.answer(f'Я забрал {botnum} конфет и победил!!! Мой приз {game.total} конфет!!! Желаешь реванша - жми /game')
                 else:
                     await message.answer(f'Я забрал {botnum} конфет. Осталось {game.number} конфет. Сколько конфет забераешь?')
         elif num.isdigit() and int(num) > game.number:
@@ -52,11 +56,7 @@ async def mes_all(message: types.Message):
         else:
             await message.answer('Мы играть будем, или ерунду друг другу писать?')
     elif not game.count: # если игра ещё не начата и не одной игры не сыграно
-        if not game.hello: # если приветствия не было
-            game.hello = True
-            await message.answer(f'Привет, {message.from_user.first_name}! В "конфеты" играть будем? Жми /start , если согласен.')
-        else:
-            await message.answer('Хорошь болтать, давай играть.\nЖми /start и погнали')
+        await message.answer('Хорошь болтать, давай играть.\nЖми /game и погнали')
     else:
-        await message.answer('Сыграем ешё разок? Жми /start , если согласен.')
+        await message.answer('Сыграем ешё разок? Жми /game , если согласен.')
         
