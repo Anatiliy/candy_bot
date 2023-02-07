45986b022f2669cd1fc92fc327536172f4a1a0d0
Anatoliy <staheev.a.n@yandex.ru>
2023-02-07 14:57:03 +0600

    добавил возможность игры несколькими игроками одновременно

--- handlers.py
+++ handlers.py
@@ -1,20 +1,24 @@
 from loader import dp
-from aiogram import types
+from aiogram.types import Message
 from candybot import CandyBot
 
-game = CandyBot()
+games = {}
+#game = CandyBot()
 
 # запуск бота
 @dp.message_handler(commands=['start'])
-async def mes_start(message: types.Message):
+async def mes_start(message: Message):
+    #print(message)
+    #print(message.from_id)
     await message.answer(f'Привет, {message.from_user.first_name}! В "конфеты" играть будем?\nУсловия игры:\nВ вазе лежит заданное количество конфет. Играют два игрока делая ход друг после друга. За один ход можно забрать не более чем 28 конфет. Все конфеты оппонента достаются сделавшему последний ход.\nЖми /game , если согласен.')
 
 # запуск игры
 @dp.message_handler(commands=['game'])
-async def mes_game(message: types.Message):
-    game.start = True
-    if game.count: # если сыграли хотябы одну игру
-        if game.win: # если в прошлой игре победил бот
+async def mes_game(message: Message):
+    games[message.from_id] = games.get(message.from_id, CandyBot())
+    games[message.from_id].start = True
+    if games[message.from_id].count: # если сыграли хотябы одну игру
+        if games[message.from_id].win: # если в прошлой игре победил бот
             await message.answer('Жаждешь реванша? Я не против. Сколько конфет положить в вазу?')      
         else:
             await message.answer('В этот раз я своего не упущу!! Сколько конфет положить в вазу?')
@@ -23,39 +27,39 @@ async def mes_game(message: types.Message):
 
 # основная логика игры
 @dp.message_handler()
-async def mes_all(message: types.Message):
+async def mes_all(message: Message):
     num = message.text
-    if game.start: # если игра начата
-        if num.isdigit() and not game.number: # если в вазе нет конфет
+    if games[message.from_id].start: # если игра начата
+        if num.isdigit() and not games[message.from_id].number: # если в вазе нет конфет
             if int(num) > 28:
-                game.total = int(num)
-                game.number = int(num)
+                games[message.from_id].total = int(num)
+                games[message.from_id].number = int(num)
                 await message.answer(f'В вазе {num} конфет. Сколько конфет забераешь?')
             else:
                 await message.answer(f'Мало, надо больше')
         elif num.isdigit() and int(num) > 28:
             await message.answer(f'Не жадничей. Не больше 28 конфет, помни об этом.')
-        elif num.isdigit() and int(num) <= game.number and int(num) < 29:
-            game.opponent(int(num))
-            if game.number == 0:
-                game.start = False
-                game.count += 1
-                game.win = 0
-                await message.answer(f'Ты победил!! Твой приз {game.total} конфет!!! Желаешь повторить триумф - Жми /game')
+        elif num.isdigit() and int(num) <= games[message.from_id].number and int(num) < 29:
+            games[message.from_id].opponent(int(num))
+            if games[message.from_id].number == 0:
+                games[message.from_id].start = False
+                games[message.from_id].count += 1
+                games[message.from_id].win = 0
+                await message.answer(f'Ты победил!! Твой приз {games[message.from_id].total} конфет!!! Желаешь повторить триумф - Жми /game')
             else:
-                botnum = game.move()
-                if game.number == 0:
-                    game.start = False
-                    game.count += 1
-                    game.win = 1
-                    await message.answer(f'Я забрал {botnum} конфет и победил!!! Мой приз {game.total} конфет!!! Желаешь реванша - жми /game')
+                botnum = games[message.from_id].move()
+                if games[message.from_id].number == 0:
+                    games[message.from_id].start = False
+                    games[message.from_id].count += 1
+                    games[message.from_id].win = 1
+                    await message.answer(f'Я забрал {botnum} конфет и победил!!! Мой приз {games[message.from_id].total} конфет!!! Желаешь реванша - жми /game')
                 else:
-                    await message.answer(f'Я забрал {botnum} конфет. Осталось {game.number} конфет. Сколько конфет забераешь?')
-        elif num.isdigit() and int(num) > game.number:
+                    await message.answer(f'Я забрал {botnum} конфет. Осталось {games[message.from_id].number} конфет. Сколько конфет забераешь?')
+        elif num.isdigit() and int(num) > games[message.from_id].number:
             await message.answer('Столько конфет нет. Возьми поменьше.')
         else:
             await message.answer('Мы играть будем, или ерунду друг другу писать?')
-    elif not game.count: # если игра ещё не начата и не одной игры не сыграно
+    elif not games[message.from_id].count: # если игра ещё не начата и не одной игры не сыграно
         await message.answer('Хорошь болтать, давай играть.\nЖми /game и погнали')
     else:
         await message.answer('Сыграем ешё разок? Жми /game , если согласен.')
