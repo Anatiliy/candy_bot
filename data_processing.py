e77d74a24873ded718f5fbe340af7c4e274951fe
Anatoliy <staheev.a.n@yandex.ru>
2023-06-26 00:55:12 +0600

    внёс маштабные изменения в класс GameData (изменил структуру данных)

--- data_processing.py
+++ data_processing.py
@@ -5,73 +5,80 @@ from random import randint
 class GameData:
 
     def __init__(self) -> None:
-        self.start = 0  # атребут пртнимает значение 1 при запуске игры и значение 0 при окончании игры
-        self.number = 0  # атребут, фиксирующий количества конфет в вазе
-        self.opponame = 'noname'  # атребут, фиксирующий имя игрока
-        self.total = 0  # атребут, фиксирующий количества конфет в игре
-        self.count = 0  # атребут, фиксирующий количество оконченых игр
-        self.win = 'None'  # атребут пртнимает значение 1 если победил бот и значение 0 если победил игрок
-        self.botbag = 300  # количество конфет в мешке бота
-        self.oppobag = 0  # количество конфет в мешке игрока
-        self.lot = 0  # количество конфет, которое можно взять из вазы
-        self.level = 0  # уровень игры
+        self.data = {'start': 0,  # атребут принимает значение 1 при запуске игры и значение 0 при окончании игры
+                     'number': 0,  # атребут, фиксирующий количества конфет в вазе
+                     'opponame': 'noname',  # атребут, фиксирующий имя игрока
+                     'total': 0,  # атребут, фиксирующий количества конфет в игре
+                     'count': 0,  # атребут, фиксирующий количество оконченых игр
+                     'win': 'None',  # атребут пртнимает значение 1 если победил бот и значение 0 если победил игрок
+                     'botbag': 300,  # количество конфет в мешке бота
+                     'oppobag': 0,  # количество конфет в мешке игрока
+                     'lot': 0,  # количество конфет, которое можно взять из вазы
+                     'level': 0}  # уровень игры
+
+
+    def __getitem__(self, item: str):
+        if item in self.data:
+            return self.data[item]
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def __setitem__(self, key: str, value):
+        self.data[key] = value
 
     # метод конвертирующий объект класса в строку
     def toStr(self):
-        return ';'.join(list(map(lambda item: str(item) if not isinstance(item, str) else item, [self.start, self.number, self.opponame, self.total, self.count, self.win, self.botbag, self.oppobag, self.lot, self.level])))
+        return ';'.join(list(map(lambda item: str(item) if not isinstance(item, str) else item, self.data.values())))
 
-    # метод конвертирующийстроку в объект класса 
-    def toCB(self, data:str):
+    # метод конвертирующий строку в объект класса
+    def toCB(self, data: str):
         lst = list(map(lambda item: int(item) if item.isdigit() else item, data))
-        self.start = lst[0]
-        self.number = lst[1]
-        self.opponame = lst[2]
-        self.total = lst[3]
-        self.count = lst[4]
-        self.win = lst[5]
-        self.botbag = lst[6]
-        self.oppobag = lst[7]
-        self.lot = lst[8]
-        self.level = lst[9]
+        index = 0
+        for key in self.data:
+            self.data[key] = lst[index]
+            index += 1
+
 
     # метод, осуществляющий ход бота
     def move(self):
-        if self.number <= self.lot:
-            result = self.number
-            self.number = 0
+        if self.data['number'] <= self.data['lot']:
+            result = self.data['number']
+            self.data['number'] = 0
             return result
-        elif self.level and self.lot + 1 < self.number <= self.lot * 2 + 1:
-            result = self.number - self.lot + 1
-            self.number = self.lot + 1
+        elif self.data['level'] and self.data['lot'] + 1 < self.data['number'] <= self.data['lot'] * 2 + 1:
+            result = self.data['number'] - self.data['lot'] + 1
+            self.data['number'] = self.data['lot'] + 1
             return result
-        elif self.level > 1 and self.number <= self.lot * 3 + 2:
-            result = self.number - self.lot * 2 + 2
-            self.number = self.lot * 2 + 2
+        elif self.data['level'] > 1 and self.data['number'] <= self.data['lot'] * 3 + 2:
+            result = self.data['number'] - self.data['lot'] * 2 + 2
+            self.data['number'] = self.data['lot'] * 2 + 2
             return result
         else:
-            result = randint(1, self.lot)
-            self.number -= result
+            result = randint(1, self.data['lot'])
+            self.data['number'] -= result
             return result
 
     # метод, осуществляющий ход игрока
     def opponent(self, oppocandy):
-        self.number -= oppocandy
+        self.data['number'] -= oppocandy
 
     # метод, осуществляющий ход игрока
     def botContribution(self, handout=0):
-        self.start = 1
-        self.lot = randint(10, 28)
+        self.data['start'] = 1
+        self.data['lot'] = randint(10, 28)
         if handout:  # если у бота нехватает конфет, и ему одолжил их игрок
-            self.total = 50
-            self.oppobag -= handout
-            self.number = self.total
-            self.botbag = 0
+            self.data['total'] = 50
+            self.data['oppobag'] -= handout
+            self.data['number'] = self.data['total']
+            self.data['botbag'] = 0
         else:
-            self.total = randint(50, 100)
-            if self.total > self.botbag:
+            self.data['total'] = randint(50, 100)
+            if self.data['total'] > self.data['botbag']:
                 self.botContribution()
-            self.number = self.total
-            self.botbag -= self.total
+            self.data['number'] = self.data['total']
+            self.data['botbag'] -= self.data['total']
 
 
 class DataProcessing:
@@ -79,12 +86,28 @@ class DataProcessing:
     def __init__(self) -> None:
         self.data = {}
 
+
+    def __getitem__(self, item: int):
+        if item in self.data:
+            return self.data[item]
+
+
+    def __len__(self):
+        return len(self.data)
+
+    def __setitem__(self, key: int, value):
+        self.data[key] = value
+
+    def __delitem__(self, key):
+        del self.data[key]
+
+
     # метод обновляет переменные для новой игры
     def newGame(self, id, name):
         self.data[id] = self.data.get(id, GameData())
-        self.data[id].start = 1
-        self.data[id].opponame = name
-        self.data[id].lot = randint(10, 28)
+        self.data[id]['start'] = 1
+        self.data[id]['opponame'] = name
+        self.data[id]['lot'] = randint(10, 28)
 
     # перезапись файла информацией из переменной
     def overwrite(self):
@@ -105,4 +128,3 @@ class DataProcessing:
                 key = int(lst.pop(0))
                 self.data[key] = GameData()
                 self.data[key].toCB(lst)
-        
