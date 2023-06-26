ab6953bf47cc835b1e4fc84e8cb61b0d5c7b22a2
Anatoliy <staheev.a.n@yandex.ru>
2023-06-26 23:27:03 +0600

    внёс незначительные изменения

--- loader.py
+++ loader.py
@@ -2,6 +2,6 @@ from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
 from os import getenv
 
-load_dotenv() # загружаем переменные из файла ".env" в виртуальное окружение
+load_dotenv()  # загружаем переменные из файла ".env" в виртуальное окружение
 bot = Bot(getenv('TOKEN'))
 dp = Dispatcher(bot)
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from os import getenv
 
 async def on_start(_):
     print('Бот запущен')
-    await dp.bot.send_message(getenv('ADMIN_ID'), 'Бот запущен.\n/start') # отправляем сообшение администратору
+    await dp.bot.send_message(getenv('ADMIN_ID'), 'Бот запущен.\n/start')  # отправляем сообшение администратору
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True, on_startup=on_start)
